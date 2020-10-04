3fbba9f5c1497efead525a352eab82de9ebc9313
formegusto <formegusto@gmail.com>
2020-10-05 01:13:34 +0900

    immer

--- src/modules/auth/reducer.ts
+++ src/modules/auth/reducer.ts
@@ -1,6 +1,7 @@
 import { CHANGE_FIELD, INITIALIZE_FORM } from './actions';
 import { createReducer } from 'typesafe-actions';
 import { Auth, AuthAction } from './types';
+import produce from 'immer';
 
 const initialState: Auth = {
     register: {
@@ -16,15 +17,13 @@ const initialState: Auth = {
 
 const auth = createReducer<Auth, AuthAction>(initialState, {
     [CHANGE_FIELD] : (state, { payload: { type, event }}) => 
-        { return { ...state, 
-            [type] : {
-                ...state[type],
-                [event.name]:event.value,
-            }} },
+        produce(state, draft => {
+            draft[type][event.name] = event.value
+        }),
     [INITIALIZE_FORM] : (state, { payload: type }) =>
-        { return { ...state,
-            [type] : initialState[type]
-        }}
+        produce(state,draft => {
+            draft[type] = initialState[type]
+        }),
 });
 
 export default auth;
